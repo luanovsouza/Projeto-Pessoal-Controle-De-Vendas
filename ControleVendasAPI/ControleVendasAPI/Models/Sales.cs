cb61807f76ebc3b4a503233c0e2dda6746ef4b96
Luan Oliveira <luanvisouza037@gmail.com>
2025-11-28 23:36:13 -0300

    Criando os Models

---
+++ ControleVendasAPI/ControleVendasAPI/Models/Sales.cs
@@ -0,0 +1,9 @@
+﻿namespace ControleVendasAPI.Models;
+
+public class Sales
+{
+    public DateTime SalesDay { get; set; }
+    public string? ClientName { get; set; }
+    public int Quantity { get; set; }
+    public double SalesPrice { get; set; }
+}
---
+++ ControleVendasAPI/ControleVendasAPI/Models/SweetKits.cs
@@ -0,0 +1,8 @@
+﻿namespace ControleVendasAPI.Models;
+
+public class SweetKits
+{
+    public string? Name { get; set; }
+    public int Quantity { get; set; }
+    public double KitPrice { get; set; }
+}
